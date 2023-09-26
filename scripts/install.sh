efab55788ec1404a7be1b5655204f65512f27fe9
Siddharth Rawat <rawat.sid@northeastern.edu>
2023-09-25 23:35:05 -0400

    fix(cd): add split() to packer config
    Introduced a local variable that will split the single string of users
    into an array of user IDs based on a specific delimiter.
    
    fixes issue #17

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,7 +13,7 @@ echo "|                                                                         
 echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
 
 # Update packages and dependencies
-sudo apt update && sudo apt upgrade -y
+sudo apt update --quiet && sudo apt upgrade -y
 
 echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
 echo "|                                                                                                                                         |"
@@ -21,25 +21,25 @@ echo "|                                                               INSTALL JA
 echo "|                                                                                                                                         |"
 echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
 
-# Installing Java 
+# Install Java for Jenkins
 sudo sudo apt install openjdk-11-jdk -y
-sleep 3
 
-echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
-echo "|                                                                                                                                         |"
-echo "|                                                             VALIDATING JAVA 11                                                          |"
-echo "|                                                                                                                                         |"
-echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
-
-# Validating java version
-JAVA=$?  
-if [ $JAVA -eq 0 ]; then  #checking if exit code is 0 or not
+# Validate Java installation
+JAVA=$?
+if [ $JAVA -eq 0 ]; then
   echo "Successfully installed Java"
 else
   echo "Unable to install Java"
 fi
+
+echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
+echo "|                                                                                                                                         |"
+echo "|                                                             CHECK JAVA VERSION                                                          |"
+echo "|                                                                                                                                         |"
+echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
+
+# Check Java version
 echo "Java $(java -version)"
-sudo apt update
 
 echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
 echo "|                                                                                                                                         |"
@@ -47,23 +47,41 @@ echo "|                                                                INSTALL J
 echo "|                                                                                                                                         |"
 echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
 
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee   /usr/share/keyrings/jenkins-keyring.asc > /dev/null	
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]   https://pkg.jenkins.io/debian-stable binary/ | sudo tee   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt update 
+# Update local package index
+sudo apt update --quiet
+
+# Debian package repository of Jenkins to automate installation and upgrade.
+# To use this repository, first add the key to the system:
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc >/dev/null
+
+# Add a Jenkins apt repository entry:
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list >/dev/null
+
+# Update local package index, then finally install Jenkins:
+sudo apt update --quiet
 sudo apt install jenkins -y
 
-echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
-echo "|                                                                                                                                         |"
-echo "|                                                             VALIDATING JENKINS                                                          |"
-echo "|                                                                                                                                         |"
-echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
-
-JENKINS=$?  
-if [ $JENKINS -eq 0 ]; then  #checking if exit code is 0 or not
+# Validate Jenkins installation
+JENKINS=$?
+if [ $JENKINS -eq 0 ]; then
   echo "Successfully installed Jenkins"
 else
   echo "Unable to install Jenkins"
 fi
+
+# Check the status of Jenkins service
+sudo systemctl status jenkins
+
+echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
+echo "|                                                                                                                                         |"
+echo "|                                                           CHECK JENKINS VERSION                                                         |"
+echo "|                                                                                                                                         |"
+echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
+
+# Check Jenkins version
 echo "Jenkins $(jenkins --version)"
 
 # Caddy Setup
@@ -73,15 +91,22 @@ echo "|                                                               INSTALL CA
 echo "|                                                                                                                                         |"
 echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
 
-sudo apt-get update
+# Update local package index
+sudo apt update --quiet
+
 sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
-sudo apt update
-sudo apt install caddy
-sleep 3
-CADDY=$?  
-if [ $CADDY -eq 0 ]; then  #checking if exit code is 0 or not
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo \
+  gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee \
+  /etc/apt/sources.list.d/caddy-stable.list
+
+# Update local package index, then finally install caddy:
+sudo apt update --quiet
+sudo apt install caddy -y
+
+# Validate caddy installation:
+CADDY=$?
+if [ $CADDY -eq 0 ]; then
   echo "Successfully installed the Caddy Service"
 else
   echo "Unable to install the Caddy Service"
