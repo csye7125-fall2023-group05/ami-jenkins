0d18eb495966de050cfac780d405b2ff42af9e08
Siddharth Rawat <rawat.sid@northeastern.edu>
2023-10-21 03:35:44 -0400

    fix: install mandatory plugins required for JCasC
    - Install `job-dsl` and `configuration-as-code-groovy` plugins
    - Add `sleep` delays
    
    Fixes issue #31

--- scripts/webapp_seed.groovy
+++ scripts/webapp_seed.groovy
@@ -2,8 +2,8 @@ multibranchPipelineJob('webapp') {
   branchSources {
     github {
       id('csye7125-webapp')
-      scanCredentialsId('jenkins-karan')
-      repoOwner('cyse7125-fall2023-group05')
+      scanCredentialsId('jenkins-sydrawat')
+      repoOwner('csye7125-fall2023-group05')
       repository('webapp')
     }
   }
