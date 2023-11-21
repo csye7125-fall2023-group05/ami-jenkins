f32d3c489ca4c549f51923b2e856214b7a13b521
Siddharth Rawat <rawat.sid@northeastern.edu>
2023-11-21 14:29:45 -0500

    feat: install required software packages for CD pipeline
    Added packages for Helm, Kubectl, GCloud CLI and GKE Auth Plugin
    
    Fixes issue #40

--- scripts/install.sh
+++ scripts/install.sh
@@ -197,3 +197,58 @@ sudo usermod -a -G docker jenkins
 
 # Check Docker version
 echo "Docker $(docker --version)"
+
+# Helm Setup: https://helm.sh/docs/intro/install/#from-apt-debianubuntu
+echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
+echo "|                                                                                                                                         |"
+echo "|                                                              INSTALL HELM                                                               |"
+echo "|                                                                                                                                         |"
+echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
+
+sudo apt-get update
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg >/dev/null
+sudo apt-get install apt-transport-https -y
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" |
+  sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update && sudo apt-get install helm
+
+# Check Helm version
+echo "Helm $(helm version)"
+
+# GCloud Setup: https://cloud.google.com/sdk/docs/install#deb
+echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
+echo "|                                                                                                                                         |"
+echo "|                                                         INSTALL GCLOUD PLUGINS                                                          |"
+echo "|                                                                                                                                         |"
+echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
+
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates gnupg curl sudo -y
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+echo \
+  "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" |
+  sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo apt-get update
+sudo apt-get install google-cloud-cli -y
+sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin -y
+
+# Check GCloud GKE Auth Plugin version
+echo "GKE Auth Plugin $(gke-cloud-auth-plugin --version)"
+
+# Kubectl Setup: https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/#install-using-native-package-management
+echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
+echo "|                                                                                                                                         |"
+echo "|                                                             INSTALL KUBECTL                                                             |"
+echo "|                                                                                                                                         |"
+echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
+
+sudo apt-get update
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key |
+  sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' |
+  sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update && sudo apt-get install kubectl -y
+
+# Check Kubectl version
+echo "Kubectl $(kubectl version --client)"
